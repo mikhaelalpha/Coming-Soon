a0d1b92b956764236834c1edc1fcf183793e80ed
mikhaelalpha <91846264+mikhaelalpha@users.noreply.github.com>
2022-05-14 11:46:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*29)
+    var links=new Array()
+    links[0]="https://open.spotify.com/track/0MHXrqn909p0LRTPsNsGEi?si=405288ed67114c7e" //move on up
+    links[1]="https://open.spotify.com/track/1NoggmVI7ReI9BzvcU2OmP?si=bc5100c8b01847aa" //that's the way of the world
+    links[2]="https://open.spotify.com/track/3vJ9nSo0zeqIAHSU697Isg?si=ad021717fb534ec0" //feelin' that the feeling's good
+    links[3]="https://open.spotify.com/track/6vNYzKRCHP6LcRh0RprXLi?si=d51b7f2076f04792" //what a wonderful thing love is
+    links[4]="https://open.spotify.com/track/3qZHUZTQiTMfJXcUYzY5mT?si=a6909c7eb8a14fd2" //in the stone
+    links[5]="https://open.spotify.com/track/7L4G39PVgMfaeHRyi1ML7y?si=8313e1ccd40c4e64" //day dreaming
+    links[6]="https://open.spotify.com/track/58r4JuwHhXLAkttkaUZfLw?si=e107ecc4ea284ba9" //got to be real
+    links[7]="https://open.spotify.com/track/5iVxkOzgZggmXx2YDi7wXy?si=2f5489244e524dd5" //high on sunshine
+    links[8]="https://open.spotify.com/track/7JFGm7ptMSB9q8BZMDUEpT?si=392bd77fad294472" //breathe your name
+    links[9]="https://open.spotify.com/track/5p9XWUdvbUzmPCukOmwoU3?si=7c740481a9224381" //suddenly i see
+    links[10]="https://open.spotify.com/track/2nGFzvICaeEWjIrBrL2RAx?si=fc66bf17b8ed4ba8" //put your records on
+    links[11]="https://open.spotify.com/track/6ybViy2qrO9sIi41EgRJgx?si=9b680fca8d97434a" //don't know why
+    links[12]="https://open.spotify.com/track/79z9QkhHePTFsSeVw9uyj0?si=075a44d74e094149" //smile
+    links[13]="https://open.spotify.com/track/0IVkP59yJ9GFF6B7IrvrxA?si=0f6a197585e6491c" //paper bag
+    links[14]="https://open.spotify.com/track/3uCkIqD0VzQUijbs8WIizs?si=252106915fc544df" //these days
+    links[15]="https://open.spotify.com/track/1upVvXlWQUwAPuLN3oh8lk?si=30744ade2a9f47d3" //talk
+    links[16]="https://open.spotify.com/track/7jvCeWOSnJs2N3spqobWnO?si=d4220f21c7494604" //skate
+    links[17]="https://open.spotify.com/track/69MZOMUPjMXaEtVtKtl32Y?si=398081ca412f4539" //tuethday
+    links[18]="https://open.spotify.com/track/69mFxANpGyq9s69LqH3d9G?si=ebd79a95f66e4773" //i love your smile
+    links[19]="https://open.spotify.com/track/0wpgLuIlkbe0vOhCbXdhZR?si=1db4af00e67e447e" //suwe ora jamu
+    links[20]="https://open.spotify.com/track/7rt1ze8gO84v4k9vBvb7JB?si=00d6147fcdc5431a" //balada insan muda
+    links[21]="https://open.spotify.com/track/06V4HMN4fXThQAQy0S6oVm?si=edbbdbbb76c8496f" //kala cinta menggoda
+    links[22]="https://open.spotify.com/track/3jjsRKEsF42ccXf8kWR3nu?si=9aafc187acb34606" //washing machine heart
+    links[23]="https://open.spotify.com/track/209ucYTS6vWEV5iydrHiOt?si=74dcde231ec442f3" //so tinha de ser com voce
+    links[24]="https://open.spotify.com/track/5XpnaZh2IQFPS5PAbm4mbe?si=d30dbfc1bafc4f51" //tristeza
+    links[25]="https://open.spotify.com/track/1TDeDFUYHjZKLsO6bzoVg1?si=951ce6b748c447d0" //open a window
+    links[26]="https://open.spotify.com/track/7hRYauk30VeBFmdeFQPNVx?si=bb0389ea22e14644" //never in your sun
+    links[27]="https://open.spotify.com/track/2O99aywAVBhaPrsiJ6zbSS?si=aa1679ec513045a9" //cause i'm a man
+    links[28]="https://open.spotify.com/track/5duZe3kHUqNENd97WYqmYL?si=8b6db104bb444520" //another lover
+    links[29]="https://open.spotify.com/track/7KYzARBusZhyZ6jygZDmVr?si=3e4924a1a19a424a" //shyness boy
+
+    window.location=links[myrandom]
+    }
